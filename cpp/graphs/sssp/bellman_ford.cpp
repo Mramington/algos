f7540e468cd3323c700d4c62001dcece58bab73a
Mramington <mramington@mail.ru>
2026-05-02 02:19:22 +0300

    feat: sssp/bellman_ford

---
+++ cpp/graphs/sssp/bellman_ford.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef struct edge
+{
+    int from;
+    int to;
+    int w;
+} edge;
+
+vector<edge> edges;
+vector<int> d;
+vector<int> prevs;
+
+bool bellman_ford(int s, int n) {
+    d.assign(n, INT_MAX);
+    prevs.assign(n, -1);
+    d[s] = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        for (const auto& [from, to, w] : edges) {
+            if (d[from] + w < d[to]) {
+                d[to] = d[from] + w;
+                prevs[to] = from;
+            }
+        }
+    }
+
+    for (const auto& [from, to, w] : edges) {
+        if (d[from] + w < d[to]) {
+            // cycle with negative weight
+            return true;
+        }
+    }
+
+    return false;
+}
